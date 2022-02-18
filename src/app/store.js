e83d4542b46e843ccff9d14441677d2f7abe6a38
Angelika Makarova <esipovaangelika@mail.ru>
2022-02-18 13:38:54 +0300

    Create topics slice, add it to the store Dispatch addTopic action

---
+++ src/app/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import topicsSliceReducer from '../features/topics/topicsSlice';
+
+export default configureStore({
+  reducer: {
+    topics: topicsSliceReducer
+  },
+});
